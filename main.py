2a2610db78df52e5d2e6158542b15b000d48e7e7
Rémi Prouteau <49786725+Treip@users.noreply.github.com>
2021-03-11 00:28:35 +0100

    Add start button
    Add the start button and connect the click to the game start

--- main.py
+++ main.py
@@ -2,10 +2,21 @@ import pygame
 import ctypes
 import game
 import math
+from pygame_widgets import Button
+
+
+
 
 # Get screen size
 user32 = ctypes.windll.user32
 screen_size = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
+
+# Init clock
+clock = pygame.time.Clock()
+FPS = 60
+
+#Init a game
+game = game.Game(1,1)
 
 # TODO Define window size
 WIDTH = 1080
@@ -18,15 +29,35 @@ pygame.init()
 pygame.display.set_caption("BomberMan")
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
+#Create the start Button 
+start_button = Button(
+            screen, WIDTH/2-50, HEIGHT/2-50, 100, 100, text='Start',
+            fontSize=50, margin=20,
+            inactiveColour=(255, 0, 0),
+            pressedColour=(0, 255, 0), radius=20,
+            onClick = lambda : game.start()     #run game.start() when the button is clicked
+         )
+
+
 # TODO Import Background
 
 # TODO Import Start button
 
-#Keep the window opened
+# Keep the window opened
 running = True
 while running:
-  for event in pygame.event.get():
+    events = pygame.event.get()
+    for event in events:
         # END condition
         if event.type == pygame.QUIT:
             running = False
             pygame.quit()
+    if(game._is_playing):
+    #When the game is not over
+        game.update(screen)
+        clock.tick(FPS)
+    else :
+    #When in the "Menu"
+        start_button.listen(events)
+        start_button.draw()
+        pygame.display.update()
