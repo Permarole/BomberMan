0568ecb6da71519027b6831df3617432f791c980
John <jonathan.permarole@hotmail.com>
2021-03-12 14:50:20 +0100

    Import bomb Update method new_bomb() Add self to bomber parameters

--- game.py
+++ game.py
@@ -2,6 +2,7 @@ import pygame
 import animation
 from soundManager import SoundManager
 from player import Player
+from bomb import Bomb
 
 
 class Game:
@@ -16,15 +17,15 @@ class Game:
         self._bonuses = pygame.sprite.Group()
         self._pressed = dict()
         self._sound_manager = SoundManager()
-        self._player = Player((0,0),"red")
+        self._player = Player(self, (0,0), "red")
         self._level = level
 
     def start(self):
         self._is_playing = True
 
-    def new_bomb(self, bomber, power=3):
+    def new_bomb(self, bomber):
         """ Create a new bomb and add it to _bombs"""
-        self._bombs.add(Bomb(bomber, power))
+        self._bombs.add(Bomb(bomber))
 
     def update(self,screen):
         keys = pygame.key.get_pressed()
