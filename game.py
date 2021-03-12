16bc5773221fb75b7f63302cd2d26ca7f14394fc
John <jonathan.permarole@hotmail.com>
2021-03-12 14:41:17 +0100

    Switch dict() to pygame Group():  - Better handling of sprites
    Create method new_bomb()

--- game.py
+++ game.py
@@ -10,17 +10,21 @@ class Game:
 
         self._size = window_size
         self._is_playing = False
-        self._bombers = dict()
-        self._bombs = dict()
-        self._fires = dict()
+        self._bombers = pygame.sprite.Group()
+        self._bombs = pygame.sprite.Group()
+        self._fires = pygame.sprite.Group()
+        self._bonuses = pygame.sprite.Group()
         self._pressed = dict()
-        self._bonuses = dict()
         self._sound_manager = SoundManager()
-        self._player = Player((0,0),"red") 
+        self._player = Player((0,0),"red")
         self._level = level
 
     def start(self):
         self._is_playing = True
+
+    def new_bomb(self, bomber, power=3):
+        """ Create a new bomb and add it to _bombs"""
+        self._bombs.add(Bomb(bomber, power))
 
     def update(self,screen):
         keys = pygame.key.get_pressed()
